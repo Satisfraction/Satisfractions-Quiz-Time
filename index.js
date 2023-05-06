097518e6d8bea0a6f58282723029f7f01bfaa899
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-05-06 17:12:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+// Get the next button element
+const nextButton = document.querySelector(".next");
+
+// Add a click event listener to the next button
+nextButton.addEventListener("click", displayQuestion);
+
+// Hide the next button initially
+nextButton.classList.add("hidden");
+
+// Define a function to check the answer
+function checkAnswer(event) {
+  // Get the clicked answer element
+  const answer = event.target;
+
+  // Get the value of the answer (0 for wrong, 1 for correct)
+  const value = answer.dataset.value;
+
+  // Get the feedback element
+  const feedback = document.querySelector(".feedback");
+
+  // If the answer is correct
+  if (value == "1") {
+    // Add the correct class to the answer
+    answer.classList.add("correct");
+
+    // Show a positive feedback message
+    feedback.textContent = "Richtig! Gut gemacht!";
+  }
+  // If the answer is wrong
+  else {
+    // Add the wrong class to the answer
+    answer.classList.add("wrong");
+
+    // Show a negative feedback message
+    feedback.textContent = "Falsch! Versuche es noch einmal!";
+  }
+
+  // Disable further clicks on the answers
+  answers.forEach(answer => {
+    answer.removeEventListener("click", checkAnswer);
+  });
+
+  // Show the next button
+  nextButton.classList.remove("hidden");
+}
+
+// Define a function to display a random question
+function displayQuestion() {
+  // Hide the next button
+  nextButton.classList.add("hidden");
+
+  // Get a random index from the questions array
+  const index = Math.floor(Math.random() * questions.length);
+
+  // Get the question object at that index
+  const question = questions[index];
+
+  // Get the question element
+  const questionElement = document.querySelector(".question");
+
+  // Set the question element's text to the question's text
+  questionElement.textContent = question.question;
+
+  // Get the answer elements
+  const answerElements = document.querySelectorAll(".answer");
+
+  // Loop through the answer elements
+  for (let i = 0; i < answerElements.length; i++) {
+    // Set the answer element's text to the answer's text
+    answerElements[i].textContent = question.answers[i].text;
+
+    // Set the answer element's data-value to the answer's value
+    answerElements[i].dataset.value = question.answers[i].value;
+
+    // Remove any previous classes from the answer element
+    answerElements[i].className = "answer";
+
+    // Add a click event listener to the answer element
+    answerElements[i].addEventListener("click", checkAnswer);
+  }
+}
+
+// Call the displayQuestion function when the page loads
+window.addEventListener("load", displayQuestion);
---
+++ questions.js
@@ -0,0 +1,157 @@
+const questions = [
+  {
+    question: "Wie heißt der größte Kontinent der Erde?",
+    answers: [
+      { text: "Europa", value: 0 },
+      { text: "Amerika", value: 0 },
+      { text: "Asien", value: 1 },
+      { text: "Afrika", value: 0 }
+    ]
+  },
+  {
+    question: "Wie heißt die Hauptstadt von Deutschland?",
+    answers: [
+      { text: "Berlin", value: 1 },
+      { text: "München", value: 0 },
+      { text: "Hamburg", value: 0 },
+      { text: "Frankfurt", value: 0 }
+    ]
+  },
+  {
+    question: "Wie heißt das schnellste Landtier?",
+    answers: [
+      { text: "Giraffe", value: 0 },
+      { text: "Gepard", value: 1 },
+      { text: "Löwe", value: 0 },
+      { text: "Nashorn", value: 0 }
+    ]
+  },
+  {
+    question: "Welches ist das kleinste Land der Welt?",
+    answers: [
+      { text: "Katar", value: 0 },
+      { text: "Monaco", value: 0 },
+      { text: "Vatikanstadt", value: 1 },
+      { text: "San Marino", value: 0 }
+    ]
+  },
+  {
+    question: "Welches Tier ist das größte auf der Erde?",
+    answers: [
+      { text: "Giraffe", value: 0 },
+      { text: "Elefant", value: 0 },
+      { text: "Nilpferd", value: 0 },
+      { text: "Wal", value: 1 }
+    ]
+  },
+  {
+    question: "Was ist die Hauptstadt von Frankreich?",
+    answers: [
+      { text: "Lyon", value: 0 },
+      { text: "Nizza", value: 0 },
+      { text: "Paris", value: 1 },
+      { text: "Marseille", value: 0 }
+    ]
+  },
+  {
+    question: "Wie viele Kontinente gibt es auf der Erde?",
+    answers: [
+      { text: "3", value: 0 },
+      { text: "5", value: 0 },
+      { text: "7", value: 7 },
+      { text: "9", value: 0 }
+    ]
+  },
+  {
+    question: "Welche Farbe hat eine Banane, wenn sie reif ist?",
+    answers: [
+      { text: "Grün", value: 0 },
+      { text: "Gelb", value: 1 },
+      { text: "Rot", value: 0 },
+      { text: "Blau", value: 0 }
+    ]
+  },
+  {
+    question: "Welcher Planet ist der dritte von der Sonne?",
+    answers: [
+      { text: "Erde", value: 1 },
+      { text: "Mars", value: 0 },
+      { text: "Jupiter", value: 0 },
+      { text: "Venus", value: 0 }
+    ]
+  },
+  {
+    question: "Wie nennt man eine Gruppe von Fischen?",
+    answers: [
+      { text: "Schwarm", value: 1 },
+      { text: "Herde", value: 0 },
+      { text: "Rudel", value: 0 },
+      { text: "Herd", value: 0 }
+    ]
+  },
+  {
+    question: "Wie viele Augen hat eine Biene?",
+    answers: [
+      { text: "2", value: 1 },
+      { text: "4", value: 0 },
+      { text: "6", value: 0 },
+      { text: "8", value: 0 }
+    ]
+  },
+  {
+    question: "Wie viele Farben hat ein Regenbogen?",
+    answers: [
+      { text: "5", value: 0 },
+      { text: "6", value: 1 },
+      { text: "7", value: 0 },
+      { text: "8", value: 0 }
+    ]
+  },
+  {
+    question: "Wie viele Beine hat eine Spinne?",
+    answers: [
+      { text: "4", value: 0 },
+      { text: "6", value: 1 },
+      { text: "8", value: 0 },
+      { text: "10", value: 0 }
+    ]
+  },
+  {
+    question: "Was ist das größte Tier der Erde?",
+    answers: [
+      { text: "Elefant", value: 0 },
+      { text: "Krokodil", value: 0 },
+      { text: "Wal", value: 1 },
+      { text: "Giraffe", value: 0 }
+    ]
+  },
+  {
+    question: "Welches Tier sagt 'Miau'?",
+    answers: [
+      { text: "Hund", value: 0 },
+      { text: "Katze", value: 1 },
+      { text: "Pferd", value: 0 },
+      { text: "Kuh", value: 0 }
+    ]
+  },
+  {
+    question: "Welches der folgenden Tiere ist ein Raubtier?",
+    answers: [
+      { text: "Kaninchen", value: 0 },
+      { text: "Eichhörnchen", value: 0 },
+      { text: "Löwe", value: 1 },
+      { text: "Kuh", value: 0 }
+    ]
+  },
+  {
+    question: "Wie viele Zähne hat ein Erwachsener?",
+    answers: [
+      { text: "16", value: 0 },
+      { text: "24", value: 0 },
+      { text: "32", value: 1 },
+      { text: "40", value: 0 }
+    ]
+  },
+
+  // platz für mehr fragen :)
+];
